a432277cbe628a127c801e1687c9c0483aa6ed96
P4anix <pancok_1@yahoo.it>
2023-03-06 19:02:07 +0100

    problema riconoscimento numeri

--- js/main.js
+++ js/main.js
@@ -14,14 +14,27 @@ function nascondi() {
     numeriVisualizzati.innerHTML = '';
 }
 
+/* Array per memorizzare i numeri inseriti */
+const numeriInseriti = [];
+
 /* Funzione per chiedere numeri all'utente */
 function appariPrompt() {
     for (let i = 0; i < 5; i++) {
         const utente = Number(prompt('Inserisci il ' + (i + 1) + ' numero che ricordi'));
+        numeriInseriti.push(utente);
     }
+
+    /* confronto numeri inseriti e generati */
+    let uguali = 0;
+
+    for (let i = 0; i < numeriInseriti.length; i++) {
+        if (numeriVisualizzati.innerHTML.includes(numeriInseriti[i])) {
+            uguali++;
+        }
+    }
+
+    alert(`Hai indovinato ${uguali} numeri su 5.`);
 }
-
-
 /*----------
     Main
  ---------*/
@@ -36,9 +49,9 @@ for (let i = 0; i < 5; i++) {
 }
 
 /* settiamo il timer per nascondere i numeri */
-const tempoNascondere = 30;
+const tempoNascondere = 10;
 setTimeout(nascondi, tempoNascondere * 1000);
 
 /* settiamo il timer per far apparire il prompt */
-const tempoPrompt = 31;
+const tempoPrompt = 11;
 setTimeout(appariPrompt, tempoPrompt * 1000);
