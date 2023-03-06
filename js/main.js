bb02366b9f97368c48ea538a062b35e745e59a0b
P4anix <pancok_1@yahoo.it>
2023-03-06 16:23:13 +0100

    generazione numeri randomici

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 'Use strict'
+
+/*----------
+  Funzioni
+ ---------*/
+
+// Funzione per generare numeri casuali
+ function getRandomNumber(max) {
+	return Math.floor(Math.random() * max) + 1;
+}
+
+/*----------
+    Main
+ ---------*/
+
+/* Generiamo i numeri casuali e mostriamoli su html */
+const numeriVisualizzati = document.getElementById('numeri')
+
+for (let i = 0; i < 5; i++) {
+    const numeri = getRandomNumber(100);
+    console.log(numeri);  
+    numeriVisualizzati.innerHTML += numeri + "<br>";
+}
