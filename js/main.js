97b0edc73808767be8962b87a27a6737bb623d3f
P4anix <pancok_1@yahoo.it>
2023-03-06 18:26:28 +0100

    prompt utente

--- js/main.js
+++ js/main.js
@@ -5,14 +5,22 @@
  ---------*/
 
 // Funzione per generare numeri casuali
- function getRandomNumber(max) {
-	return Math.floor(Math.random() * max) + 1;
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max) + 1;
 }
 
 /* Funzione per nascondere i numeri */
 function nascondi() {
     numeriVisualizzati.innerHTML = '';
 }
+
+/* Funzione per chiedere numeri all'utente */
+function appariPrompt() {
+    for (let i = 0; i < 5; i++) {
+        const utente = Number(prompt('Inserisci il ' + (i + 1) + ' numero che ricordi'));
+    }
+}
+
 
 /*----------
     Main
@@ -23,10 +31,14 @@ const numeriVisualizzati = document.getElementById('numeri')
 
 for (let i = 0; i < 5; i++) {
     const numeri = getRandomNumber(100);
-    console.log(numeri);  
+    console.log(numeri);
     numeriVisualizzati.innerHTML += numeri + "<br>"; /* brutto a livelli altissimi */
 }
 
 /* settiamo il timer per nascondere i numeri */
-const tempoNascondere = 10
-setTimeout (nascondi, tempoNascondere * 1000);
+const tempoNascondere = 30;
+setTimeout(nascondi, tempoNascondere * 1000);
+
+/* settiamo il timer per far apparire il prompt */
+const tempoPrompt = 31;
+setTimeout(appariPrompt, tempoPrompt * 1000);
