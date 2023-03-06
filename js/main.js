9213dd05963cb4b76e7730e5bb27d6b917c1f904
P4anix <pancok_1@yahoo.it>
2023-03-06 21:01:51 +0100

    esercizio done ma brutto

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   Funzioni
  ---------*/
 
-// Funzione per generare numeri casuali
+/* Funzione per generare numeri casuali */
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max) + 1;
 }
@@ -14,43 +14,45 @@ function nascondi() {
     numeriVisualizzati.innerHTML = '';
 }
 
-/* Array per memorizzare i numeri inseriti */
-const numeriInseriti = [];
-
-/* Funzione per chiedere numeri all'utente */
+/* Funzione per chiedere numeri all'utente e confrontarli con i numeri casuali */
 function appariPrompt() {
+    const numeriInseriti = [];
+    let uguali = 0;
+
     for (let i = 0; i < 5; i++) {
         const utente = Number(prompt('Inserisci il ' + (i + 1) + ' numero che ricordi'));
         numeriInseriti.push(utente);
-    }
-
-    /* confronto numeri inseriti e generati */
-    let uguali = 0;
-
-    for (let i = 0; i < numeriInseriti.length; i++) {
-        if (numeriVisualizzati.innerHTML.includes(numeriInseriti[i])) {
+        if (numeriCasuali.includes(utente)) {
             uguali++;
         }
     }
 
     alert(`Hai indovinato ${uguali} numeri su 5.`);
 }
+
 /*----------
     Main
  ---------*/
+
+/* Array per memorizzare i numeri casuali */
+const numeriCasuali = [];
 
 /* Generiamo i numeri casuali e mostriamoli su html */
 const numeriVisualizzati = document.getElementById('numeri')
 
 for (let i = 0; i < 5; i++) {
-    const numeri = getRandomNumber(100);
+    const numeri = getRandomNumber(90);
+    numeriCasuali.push(numeri);
     console.log(numeri);
-    numeriVisualizzati.innerHTML += numeri + "<br>"; /* brutto a livelli altissimi */
+    const nuovoDiv = document.createElement('div');
+    nuovoDiv.innerText = numeri;
+    numeriVisualizzati.append(nuovoDiv);
 }
 
 /* settiamo il timer per nascondere i numeri */
 const tempoNascondere = 10;
 setTimeout(nascondi, tempoNascondere * 1000);
+
 
 /* settiamo il timer per far apparire il prompt */
 const tempoPrompt = 11;
